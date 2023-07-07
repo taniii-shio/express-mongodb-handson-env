333e9d7c450c14ff06c881c1b89440e9dedac186
Naoto Shioya <naoto.shioya@Frusciante.local>
2023-07-07 15:36:40 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+const MongoClient = require("mongodb").MongoClient;
+
+const PORT = process.env.PORT || 8000;
+const CONNECTION_URI = "mongodb://mongo:27017/";
+// const DATABASE = "weblog";
+const OPTIONS = {
+  family: 4,
+};
+
+MongoClient.connect(CONNECTION_URI, [OPTIONS], (err, client) => {
+  // var db = client.db(DATABASE);
+  if (err) throw err;
+  console.log("mongodb connected");
+  client.close();
+});
+
+// respond with "hello express" when a GET request is made to the homepage
+app.get("/", (req, res) => {
+  res.send("hello express");
+});
+
+// // run server
+app.listen(PORT, () => {
+  console.log(`server running at port ${PORT}`);
+});
